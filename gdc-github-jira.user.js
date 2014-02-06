6aa9addf3015e198ce607f13b351e7dbf0eab4f5
Martin Caslavsky <martin.caslavsky@gooddata.com>
2014-02-06 22:07:03 +0100

    Add gdc-github-jira

---
+++ gdc-github-jira.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        gdc-github-jira
+// @namespace   http://intgdc.com
+// @description GDC GitHub JIRA integration
+// @include     http://github.com/gooddata/*
+// @include     https://github.com/gooddata/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+textNodes = document.evaluate("//*[not(self::a)]/text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+var searchRE = new RegExp('([A-Z]{3,}\-[0-9]{3,})','g');
+var jiraReplacementDiv = document.createElement('div');
+for (var i=0;i<textNodes.snapshotLength;i++) {
+        var node = textNodes.snapshotItem(i);
+        if(searchRE.test(node.data)) {
+                jiraReplacementDiv.innerHTML = node.data.replace(searchRE, "<a href=\"https://jira.intgdc.com/browse/$1\">$1</a>");
+                while (jiraReplacementDiv.firstChild) {
+                node.parentNode.insertBefore(jiraReplacementDiv.firstChild, node);
+        }
+        node.parentNode.removeChild(node);
+    }
+}
