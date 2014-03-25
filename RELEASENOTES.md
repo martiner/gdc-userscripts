7f60178d7ef5b6bee1751f41e3adc220526b42e3
Li Ry <rysavylibor@gmail.com>
2014-03-25 20:06:12 +0100

    Exlude textarea, input and title elements from replacements

---
+++ RELEASENOTES.md
@@ -0,0 +1,7 @@
+# Release Notes
+
+## 2
+* Exlude textarea, input and title elements from replacements
+
+## 1
+* Initial version
--- gdc-github-jira.user.js
+++ gdc-github-jira.user.js
@@ -4,10 +4,10 @@
 // @description GDC GitHub JIRA integration
 // @include     http://github.com/gooddata/*
 // @include     https://github.com/gooddata/*
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
-textNodes = document.evaluate("//*[not(self::a)]/text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+textNodes = document.evaluate("//*[not(self::a) and not(self::textarea) and not(self::input) and not(self::title)]/text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
 var searchRE = new RegExp('([A-Z]{3,}\-[0-9]{3,})','g');
 var jiraReplacementDiv = document.createElement('div');
 for (var i=0;i<textNodes.snapshotLength;i++) {
